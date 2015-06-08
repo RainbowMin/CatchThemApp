59315f90f2d2c793d0c4029599a04c042462b37d
RainbowMin <123438952@qq.com>
2015-06-08 17:59:07 +0800

    测试了一下调用摄像头的api，android上测试通过

--- Assets/Scripts/BusinessLogic/CameraUsage.cs
+++ Assets/Scripts/BusinessLogic/CameraUsage.cs
@@ -9,15 +9,32 @@ using System.Collections;
 
 public class CameraUsage : MonoBehaviour 
 {
-
+    public string _DeviceName;//这个虽然public，但无需为其绑定变量，直接运行，默认调用，显示本地摄像机的名称
+    WebCamTexture _WebCam;
 
 	void Start () 
 	{
-	
+        StartCoroutine(WebCam_Play());
 	}
-	
-	void Update () 
-	{
-	
-	}
+
+    IEnumerator WebCam_Play()
+    {
+        yield return Application.RequestUserAuthorization(UserAuthorization.WebCam);
+
+        if (Application.HasUserAuthorization(UserAuthorization.WebCam))
+        {
+            WebCamDevice[] devices = WebCamTexture.devices;
+            _DeviceName = devices[0].name;
+            _WebCam = new WebCamTexture(_DeviceName, 400, 300, 12);
+            renderer.material.mainTexture = _WebCam;
+
+            _WebCam.Play();
+        }
+    }
+
+    //void Update()
+    //{
+    //    MeshRenderer renderer = this.GetComponent<MeshRenderer>();
+    //    renderer.material.mainTexture = _WebCam;
+    //}
 }

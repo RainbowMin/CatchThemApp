b46bd2fcc4161ad8d91e8246013b40e8037d70fe
RainbowMin <123438952@qq.com>
2015-06-08 15:27:38 +0800

    代码目录结构整理

---
+++ Assets/Scripts/BusinessLogic/CameraUsage.cs
@@ -0,0 +1,23 @@
+﻿/********************************************************************
+	created:	2015/6/8
+	filename: 	CameraUsage
+	author:		Rainbow	
+	purpose:	
+*********************************************************************/
+using UnityEngine;
+using System.Collections;
+
+public class CameraUsage : MonoBehaviour 
+{
+
+
+	void Start () 
+	{
+	
+	}
+	
+	void Update () 
+	{
+	
+	}
+}
